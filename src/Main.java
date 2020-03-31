c9b8eebc8eee832296b70fe1f202bc31d4ee122a
marcheimendinger <marc.heimendinger@me.com>
2020-03-31 18:09:13 +0200

    Add window size constraint

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("MainMenu");
+        JFrame frame = new JFrame("Puissance4");
         frame.setContentPane(new MainMenu().MenuPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setMinimumSize(new Dimension(600, 550));
         frame.setVisible(true);
     }
+
 }
