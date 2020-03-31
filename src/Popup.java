567c128b22a58125622ff0e2e47c2b3c70ba09b3
marcheimendinger <marc.heimendinger@me.com>
2020-03-31 18:57:48 +0200

    Add Popup implementation

--- src/Popup.java
+++ src/Popup.java
@@ -1,34 +1,57 @@
-import java.beans.EventHandler;
+import javax.swing.*;
+import java.awt.*;
 
 public class Popup {
 
+    Component parentComponent;
     String title;
     String message;
-    Button buttonLeft;
-    Button buttonRight;
+    String buttonLeftTitle;
+    String buttonRightTitle;
+    int choice = -1;
+
+    enum Choice {
+        UNKNOWN,
+        LEFT,
+        RIGHT
+    }
 
     public Popup(
+            Component parentComponent,
             String title,
             String message,
             String buttonLeftTitle,
-            String buttonRightTitle,
-            EventHandler buttonLeftEventHandler,
-            EventHandler buttonRightEventHandler
+            String buttonRightTitle
     ) {
+        this.parentComponent = parentComponent;
         this.title = title;
         this.message = message;
-        buttonLeft = new Button(buttonLeftTitle);
-        buttonRight = new Button(buttonRightTitle);
-        buttonLeft.setOneMouseClicked(buttonLeftEventHandler);
-        buttonRight.setOneMouseClicked(buttonRightEventHandler);
+        this.buttonLeftTitle = buttonLeftTitle;
+        this.buttonRightTitle = buttonRightTitle;
     }
 
     public void show() {
-        // TODO
+        Object[] options = {buttonRightTitle, buttonLeftTitle};
+        choice = JOptionPane.showOptionDialog(
+                parentComponent,
+                message,
+                title,
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[0]);
     }
 
-    public void hide() {
-        // TODO
+    public Choice getChoice() {
+        switch (choice) {
+            case 0:
+                return Choice.RIGHT;
+            case 1:
+                return Choice.LEFT;
+            default:
+                return Choice.UNKNOWN;
+        }
     }
 
 }
